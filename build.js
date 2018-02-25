cef9e8c2c366755663b11fb189999d5d8e0b73a2
DESKTOP-166QB0J\samoh <samytheseal@hotmail.com>
2018-02-25 12:34:39 +1100

    working build

--- build.js
+++ build.js
@@ -1,5 +1,3 @@
-const pathvars = require('./gulp/pathvars.js')
-
 const metalsmith = require('metalsmith');
 const drafts = require('metalsmith-drafts');
 const markdown = require('metalsmith-markdown');
@@ -9,8 +7,12 @@ const layouts = require('metalsmith-layouts');
 const sitemap = require('metalsmith-sitemap');
 const Handlebars = require('handlebars');
 const moment = require('moment');
-const gulpsmith = require('gulpsmith');
-const hb = require('gulp-hb');
+const hbHelpers = require('handlebars-helpers');
+const hbLayouts = require('handlebars-layouts');
+
+
+Handlebars.registerHelper(hbHelpers(Handlebars));
+Handlebars.registerHelper(hbLayouts(Handlebars));
 
 Handlebars.registerHelper('is', function (value, test, options) {
     if (value === test) {
@@ -19,7 +21,6 @@ Handlebars.registerHelper('is', function (value, test, options) {
         return options.inverse(this);
     }
 });
-
 Handlebars.registerHelper('date', function (date) {
     return moment(date, "MM-DD-YYYY").format('Do MMM \'YY');
 });
@@ -63,43 +64,6 @@ metalsmith(__dirname)
     .use(sitemap({
         hostname: "https://www.test.com"
     }))
-    .use(gulpsmith()
-        var timestamp = new Date().toISOString()
-        timestamp = timestamp.replace(/:|\./g, '-')
-        return gulp
-            .src(pathvars.paths.viewsStatic.src)
-            // .pipe($.dataJson())
-            .pipe(hb()
-                .data(require('./' + pathvars.paths.viewsStatic.srcFolder + 'data/components.config.js'))
-                .data(require('./' + pathvars.paths.viewsStatic.srcFolder + 'data/data.config.js'))
-                .data(require('./' + pathvars.paths.viewsStatic.srcFolder + 'data/route.config.js'))
-                .helpers(require('handlebars-helpers'))
-                .helpers(require('handlebars-layouts'))
-                .helpers({
-                    partial: partial => {
-                        return partial
-                    },
-                    repeat: (n, block) => {
-                        var accum = ''
-                        for (var i = 0; i < n; ++i) {
-                            block.data.index = i
-                            block.data.first = i === 0
-                            block.data.last = i === n - 1
-                            accum += block.fn(this)
-                        }
-                        return accum
-                    }
-                })
-                .partials('./' + pathvars.paths.viewsStatic.srcFolder + 'partials/**/*.hbs')
-            )
-            .pipe($.replace('!TIMESTAMP!', timestamp))
-            .pipe(
-                gulp.dest(pathvars.paths.viewsStatic.dist).on('end', () => {
-                    $.del(pathvars.paths.viewsStatic.dist + 'partials/')
-                })
-            )
-        )
-    )
     .build(function (err) {
         if (err) throw err;
     });
